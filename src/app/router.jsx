c295878fea6b2fde2d7583e20a991da45da75dc6
Eric Furspan <eric.furspan@gmail.com>
2020-05-30 01:41:10 -0400

    Add Privacy policy and TOS

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Login, ForgotPassword, Dashboard } from '../views';
+import { Login, ForgotPassword, Dashboard, TermsOfService, PrivacyPolicy } from '../views';
 
 const AppRouter = ({ auth }) => (
   <Switch>
+    <Route exact path="/terms" render={() => <TermsOfService />} />
+    <Route exact path="/privacy" render={() => <PrivacyPolicy />} />
     <Route exact path="/forgot-password" render={() => <ForgotPassword />} />
     <Route render={() => (auth.isAuthenticated() ? <Dashboard /> : <Login />)} />
   </Switch>
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,5 @@
 export { Login } from './Login';
 export { ForgotPassword } from './ForgotPassword';
 export { Dashboard } from './Dashboard';
+export { TermsOfService } from './Terms';
+export { PrivacyPolicy } from './Privacy';
