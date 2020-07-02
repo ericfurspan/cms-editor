5b5f0206144f848a02c11db25fb64fb3e83dabb1
Eric Furspan <eric.furspan@gmail.com>
2020-07-01 21:12:10 -0400

    Use fragments in gql query definitions

--- src/graphql/business.js
+++ src/graphql/business.js
@@ -1,36 +1,47 @@
 import { gql } from '@apollo/client';
 
+const businessFragment = gql`
+  fragment BusinessFragment on Business {
+    id
+    name
+    caption
+    logo {
+      name
+      url
+      alternativeText
+    }
+    business_email
+    social_media_links
+    payment_links
+    podcast_links
+    business_hours
+    mission_statement
+    events
+    news
+    gallery {
+      name
+      url
+      alternativeText
+    }
+  }
+`;
+
 export const FETCH_BUSINESSES = gql`
   query FetchBusinesses($where: JSON!) {
     businesses(where: $where) {
-      id
-      name
-      caption
-      business_email
-      social_media_links
-      payment_links
-      podcast_links
-      business_hours
-      mission_statement
-      events
-      news
-      gallery {
-        name
-        url
-        alternativeText
-      }
+      ...BusinessFragment
     }
   }
+  ${businessFragment}
 `;
 
 export const UPDATE_BUSINESS = gql`
   mutation UpdateBusiness($input: updateBusinessInput) {
     updateBusiness(input: $input) {
       business {
-        name
-        caption
-        mission_statement
+        ...BusinessFragment
       }
     }
   }
+  ${businessFragment}
 `;
