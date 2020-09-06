870f386bfc558c86da77f4bf05722f95b0df806e
Eric Furspan <eric.furspan@gmail.com>
2020-09-06 00:01:41 -0400

    Add autocomplete attributes to Login form

--- src/views/Login/components/LoginForm/view.jsx
+++ src/views/Login/components/LoginForm/view.jsx
@@ -51,6 +51,7 @@ const LoginForm = ({ login }) => {
           onChange={onChangeField}
           isInvalid={formError}
           required
+          autocomplete="username"
         />
       </Form.Group>
       <Form.Group controlId="password">
@@ -62,6 +63,7 @@ const LoginForm = ({ login }) => {
           onChange={onChangeField}
           isInvalid={formError}
           required
+          autocomplete="current-password"
         />
         {formError && (
           <Form.Control.Feedback className="mt-1 mb-1" type="invalid">
