2b88da6641a3edbb39767628a3ad5d92a64de3b0
Eric Furspan <eric.furspan@gmail.com>
2020-09-07 18:08:40 -0400

    Setup Sentry

--- src/app/index.entry.jsx
+++ src/app/index.entry.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { registerServiceWorker } from '../utils/sw';
 import Application from './template';
 
 library.add(fas);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 0.5,
+  environment: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+});
 
 ReactDOM.render(<Application />, document.getElementById('root'));
 
